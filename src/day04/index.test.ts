27e9ee112a84ce7875b1f5a652372713cdf12fa6
A. Fox <anthonyfox1988@gmail.com>
2022-11-16 11:05:06 -0800

    day04 part 1

--- src/day04/index.test.ts
+++ src/day04/index.test.ts
@@ -1,5 +1,23 @@
-describe("Thing", () => {
-  test("thing", () => {
-    expect(42).toBe(42)
+import {
+  hexFromString,
+  findLowestPositiveNumberWhoseHashStartsWithFiveZeros,
+} from "."
+
+describe("MD5s, Hexes, Oh my!", () => {
+  test("convert string to md5 to hex", () => {
+    expect(hexFromString("yzbqklnj1")).toBe("80012351ace7be002d4ede3bcefecab3")
+    expect(hexFromString("pqrstuv1048970")).toMatch(/000006136ef.*$/)
+  })
+
+  test("findLowestPositiveNumberWhoseHashStartsWithFiveZeros", () => {
+    expect(findLowestPositiveNumberWhoseHashStartsWithFiveZeros("abcdef")).toBe(
+      609043
+    )
+  })
+
+  test("findLowestPositiveNumberWhoseHashStartsWithFiveZeros", () => {
+    expect(
+      findLowestPositiveNumberWhoseHashStartsWithFiveZeros("pqrstuv")
+    ).toBe(1048970)
   })
 })
--- src/day04/index.ts
+++ src/day04/index.ts
@@ -1,4 +1,23 @@
+import { createHash } from "crypto"
+
+export function hexFromString(input: string): string {
+  return createHash("md5").update(Buffer.from(input)).digest("hex").toString()
+}
+
+export function findLowestPositiveNumberWhoseHashStartsWithFiveZeros(
+  secretKey: string
+) {
+  for (let index = 0; index < 10_000_000; ++index) {
+    if (hexFromString(`${secretKey}${index}`).startsWith("00000")) {
+      return index
+    }
+  }
+}
+
 export default {
-  partOne: () => { },
-  partTwo: () => { },
+  partOne: () => {
+    const secretKey = "yzbqklnj"
+    return findLowestPositiveNumberWhoseHashStartsWithFiveZeros(secretKey)
+  },
+  partTwo: () => {},
 }
