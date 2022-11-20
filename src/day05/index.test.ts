5af915987f03e1f7baacd20c4625ba870316af23
A. Fox <anthonyfox1988@gmail.com>
2022-11-20 10:25:07 -0600

    day 5 part 1

--- src/day05/index.test.ts
+++ src/day05/index.test.ts
@@ -1,5 +1,28 @@
-describe("", () => {
-  test("nothing", () => {
-    expect(true).toBeTruthy()
+import {
+  stringContainsThreeVowels,
+  stringContainsRepeatingCharacter,
+  stringContainsOffensiveSubstring,
+  stringIsNice,
+} from "."
+
+describe("stringIsNice", () => {
+  test("string contains three vowels", () => {
+    expect(stringContainsThreeVowels("ugknbfddgicrmopn")).toBe(true)
+    expect(stringContainsThreeVowels("bbb")).toBe(false)
+  })
+  test("string contains repeating characters", () => {
+    expect(stringContainsRepeatingCharacter("aaa")).toBe(true)
+    expect(stringContainsRepeatingCharacter("jchzalrnumimnmhp")).toBe(false)
+  })
+  test("string does not contain offensive substrings", () => {
+    expect(stringContainsOffensiveSubstring("haegwjzuvuyypxyu")).toBe(true)
+    expect(stringContainsOffensiveSubstring("jchzalrnumimnmhp")).toBe(false)
+  })
+  test("stringIsNice", () => {
+    expect(stringIsNice("ugknbfddgicrmopn")).toBe(true)
+    expect(stringIsNice("aaa")).toBe(true)
+    expect(stringIsNice("jchzalrnumimnmhp")).toBe(false)
+    expect(stringIsNice("haegwjzuvuyypxyu")).toBe(false)
+    expect(stringIsNice("dvszwmarrgswjxmb")).toBe(false)
   })
 })
--- src/day05/index.ts
+++ src/day05/index.ts
@@ -1,6 +1,68 @@
+import { readFileSync } from "fs"
+
+export function stringContainsThreeVowels(input: string): boolean {
+  let sumOfVowels = 0
+  const vowels = ["a", "e", "i", "o", "u"]
+  for (let val of input.split("")) {
+    if (vowels.includes(val)) {
+      ++sumOfVowels
+    }
+    if (sumOfVowels === 3) {
+      return true
+    }
+  }
+  return false
+}
+
+export function stringContainsRepeatingCharacter(input: string): boolean {
+  let lastCharacter = ""
+  for (let val of input.split("")) {
+    if (val === lastCharacter) {
+      return true
+    }
+    lastCharacter = val
+  }
+  return false
+}
+
+export function stringContainsOffensiveSubstring(input: string): boolean {
+  let lookup = new Map<string, string>([
+    ["b", "a"],
+    ["d", "c"],
+    ["q", "p"],
+    ["y", "x"],
+  ])
+
+  let lastCharacter = ""
+  for (let val of input.split("")) {
+    if (lookup.has(val) && lookup.get(val) === lastCharacter) {
+      return true
+    }
+    lastCharacter = val
+  }
+  return false
+}
+
+export function stringIsNice(input: string): boolean {
+  return (
+    stringContainsThreeVowels(input) &&
+    stringContainsRepeatingCharacter(input) &&
+    !stringContainsOffensiveSubstring(input)
+  )
+}
+
 export default {
   partOne: () => {
-    return
+    let count = 0
+    readFileSync("src/day05/input.txt")
+      .toString()
+      .split("\n")
+      .forEach((val) => {
+        if (stringIsNice(val)) {
+          ++count
+        }
+      })
+    return count
   },
   partTwo: () => {
     return
