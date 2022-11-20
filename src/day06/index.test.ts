65479f99fb26ea63ffcf23786bbb7073ab70dc3b
A. Fox <anthonyfox1988@gmail.com>
2022-11-20 14:24:46 -0600

    Add day06 structure

---
+++ src/day06/index.test.ts
@@ -0,0 +1,5 @@
+describe("", () => {
+  test("Nothing", () => {
+    expect(false).toBe(true)
+  })
+})
---
+++ src/day06/index.ts
@@ -0,0 +1,12 @@
+import { readFileSync } from "fs"
+
+export default {
+  partOne: () => {
+    readFileSync("src/day06/input.txt").toString()
+    return
+  },
+  partTwo: () => {
+    readFileSync("src/day06/input.txt").toString()
+    return
+  },
+}
