a6f4c29fee3c510a830c528fe4ba410ee215af72
A. Fox <anthonyfox1988@gmail.com>
2022-11-24 09:54:01 -0800

    day 6 wip

--- src/day06/index.test.ts
+++ src/day06/index.test.ts
@@ -1,5 +1,21 @@
-describe("", () => {
-  test("Nothing", () => {
-    expect(false).toBe(true)
+import { buildGrid, countLightsOn, Light } from "."
+
+describe("Day 6", () => {
+  test("buildGrid", () => {
+    const grid = buildGrid(2)
+
+    expect(grid.length).toBe(2)
+    expect(grid[0].length).toBe(2)
+    expect(grid[0][1]).toBeInstanceOf(Light)
+  })
+
+  test("countLightsOn", () => {
+    const grid = buildGrid(2)
+
+    grid[0][0].isOn = true
+    expect(countLightsOn(grid)).toBe(1)
+
+    grid[0][1].isOn = true
+    expect(countLightsOn(grid)).toBe(2)
   })
 })
--- src/day06/index.ts
+++ src/day06/index.ts
@@ -1,12 +1,46 @@
 import { readFileSync } from "fs"
+import { parse } from "path"
+
+export class Light {
+  public isOn: boolean
+  constructor() {
+    this.isOn = false
+  }
+}
+
+export function buildGrid(size = 1000): Light[][] {
+  return new Array<Light[]>(size).fill([]).map(() => {
+    return new Array<Light>(size).fill(null).map(() => new Light())
+  })
+}
+
+export function printGrid(grid: Light[][]) {
+  for (const row of grid) {
+    for (const cell of row) {
+      console.log(cell)
+    }
+  }
+}
+
+export function countLightsOn(grid: Light[][]): number {
+  let sum = 0
+  for (const row of grid) {
+    for (const cell of row) {
+      const amount = cell.isOn ? 1 : 0
+      sum += amount
+    }
+  }
+  return sum
+}
+
+export function handleInstruction() {}
 
 export default {
   partOne: () => {
     readFileSync("src/day06/input.txt").toString()
-    return
+    buildGrid(3)
   },
   partTwo: () => {
     readFileSync("src/day06/input.txt").toString()
-    return
   },
 }
